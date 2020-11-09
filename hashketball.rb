60e561e35c78223c00751a2f04064ca788fa1b1e
Elvis Landi <elandi13@yahoo.com>
2020-11-09 21:27:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,10 +11,10 @@ def game_hash
           number: 0,
           shoe: 16,
           points: 22, 
-          rebounds: 12
-          assists: 12
-          steals: 3 
-          blocks: 1
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
           slam dunks: 1
         }
         
