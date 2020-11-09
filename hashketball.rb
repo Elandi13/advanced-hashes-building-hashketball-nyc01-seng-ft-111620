602f9065a3af61bc7e781948b2e43363aff0b21c
Elvis Landi <elandi13@yahoo.com>
2020-11-09 21:26:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,15 @@ def game_hash
       colors: ["Black", "White"],
       players: [
         {
-          
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22, 
+          rebounds: 12
+          assists: 12
+          steals: 3 
+          blocks: 1
+          slam dunks: 1
         }
         
       
